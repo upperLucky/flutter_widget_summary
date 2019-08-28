e43845e2d297153231912ffbcaceb5f32da5f19b
upperlucky <upperLucky@gmail.com>
2019-08-28 16:23:50 +0800

    手势

---
+++ lib/basic/other/gesture_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class GesturePage extends StatefulWidget {
+  @override
+  _GesturePageState createState() => _GesturePageState();
+}
+
+class _GesturePageState extends State<GesturePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
